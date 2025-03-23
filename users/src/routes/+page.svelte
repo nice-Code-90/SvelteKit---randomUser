3974d2acb5a957f1982f424d325e4ad7fc66c7ca
Milan Mazik <mazik1990@hotmail.com>
2025-03-23 23:21:34 +0100

    first commit

---
+++ users/src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+
+</script>
+
+<h1 class="title">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<style>
+    .title{
+        color: brown;
+    }
+</style>
