b2479db40841be7efef62ddb28f5057734d67a81
Milan Mazik <mazik1990@hotmail.com>
2025-04-06 18:17:19 +0200

    basic reactivity

--- users/src/routes/+page.svelte
+++ users/src/routes/+page.svelte
@@ -1,12 +1,10 @@
 <script>
-
+	let title = $state('Welcome to SvelteKit world. This is dynamic');
+	let text = $state('This is my first component');
 </script>
 
-<h1 class="title">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>{title}</h1>
+<h2 onclick={() => alert('you clicked the message')}>{text}</h2>
 
 <style>
-    .title{
-        color: brown;
-    }
 </style>
