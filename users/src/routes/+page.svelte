e7c9e8627c035a08f0965e547798b6a9a43d8028
Milan Mazik <mazik1990@hotmail.com>
2025-04-06 21:00:23 +0200

    update title

--- users/src/routes/+page.svelte
+++ users/src/routes/+page.svelte
@@ -1,10 +1,23 @@
 <script>
-	let title = $state('Welcome to SvelteKit world. This is dynamic');
-	let text = $state('This is my first component');
+	let title = $state('Welcome to svelteKit world');
+	let text = $state('This is my first project');
+
+	function update() {
+		setTimeout(() => {
+			title = title + 'This is reactive.';
+		}, 4000);
+	}
+	update();
 </script>
 
 <h1>{title}</h1>
-<h2 onclick={() => alert('you clicked the message')}>{text}</h2>
+<h2
+	onclick={() => {
+		alert('clicked');
+	}}
+>
+	{text}
+</h2>
 
 <style>
 </style>
