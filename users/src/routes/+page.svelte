5ac5ab3d7085937d3cd6929b8ba3baa24073fc33
Milan Mazik <mazik1990@hotmail.com>
2025-04-06 22:14:57 +0200

    single user added

--- users/src/routes/+page.svelte
+++ users/src/routes/+page.svelte
@@ -1,23 +1,40 @@
 <script>
-	let title = $state('Welcome to svelteKit world');
-	let text = $state('This is my first project');
-
-	function update() {
-		setTimeout(() => {
-			title = title + 'This is reactive.';
-		}, 4000);
-	}
-	update();
 </script>
 
-<h1>{title}</h1>
-<h2
-	onclick={() => {
-		alert('clicked');
-	}}
->
-	{text}
-</h2>
+<div class="contact-card">
+	<img
+		src="https://i0.wp.com/www.lifewaycenters.com/wp-content/uploads/2016/06/placeholder-150x150-1.png?fit=150%2C150&ssl=1"
+		alt="profile"
+	/>
+
+	<div class="user-details">
+		<p>Name: John Doe</p>
+		<p>Email: iamnotexistingperson@writeme.com</p>
+		<p>Age: 30</p>
+	</div>
+</div>
 
 <style>
+	.contact-card {
+		display: flex;
+		padding: 10px;
+		color: #ffffff;
+		background-color: rgb(42, 84, 104);
+		font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+		box-shadow: 10px 10px 25px -16px rgba(0, 0, 0, 0.75);
+		border-radius: 10px;
+		max-width: 500px;
+		max-height: 125px;
+		margin-bottom: 10px;
+	}
+
+	.contact-card p {
+		margin-left: 10px;
+		margin-top: 0;
+	}
+
+	button {
+		margin-left: 10px;
+		margin-bottom: 10px;
+	}
 </style>
