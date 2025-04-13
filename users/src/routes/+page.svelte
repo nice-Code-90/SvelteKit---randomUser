00c8b47dd91edc14e9e5d7bed9cdefdfd8cbe8a2
Milan Mazik <mazik1990@hotmail.com>
2025-04-13 23:08:52 +0200

    .

--- users/src/routes/+page.svelte
+++ users/src/routes/+page.svelte
@@ -2,6 +2,6 @@
 	import UserProfile from '$lib/Components/UserProfile.svelte';
 </script>
 
-<UserProfile name="John Doe" email="john@someone.com" age={33}></UserProfile>
-<UserProfile name="John Doe" email="john@someone.com" age={33}></UserProfile>
-<UserProfile name="John Doe" email="john@someone.com" age={33}></UserProfile>
+<UserProfile name="John Doe" email="john@someone.com" age={40}></UserProfile>
+<UserProfile name="Jane Doe" email="jane@notexisting.com" age={33}></UserProfile>
+<UserProfile name="Harry Potter" email="harry@hogwarts.com" age={14}></UserProfile>
